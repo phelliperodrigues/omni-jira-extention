515f3b8e577409af3898dabfc49e6d6a14699494
Phellipe <phellipe.rodrigues@omni.com.br>
2022-06-10 12:01:41 -0300

    Add validation if error

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function openNewTabFromCardNumber() {
-       
-        window.open(generateLink(), '_blank');
+        var link = generateLink();
+        if (link) {
+            window.open(link, '_blank');
+        } else {
+            var x = document.getElementById("toasty");
+            x.className = "show";
+            setTimeout(function () { x.className = x.className.replace("show", ""); }, 1000);
+        }
     }
 
     document.querySelector('#btn').addEventListener('click', function () {
@@ -23,23 +29,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function generateLink() {
         var cardNumber = document.querySelector('#card-number').value;
+        if (cardNumber.length === 0) {
+            return undefined;
+        }
         var prefix = document.querySelector('#prefix').value;
         return 'https://omnicfi.atlassian.net/browse/' + prefix + '-' + cardNumber;
     }
 
     document.querySelector('#btn-copy').addEventListener('click', function () {
         var link = generateLink();
-        if (navigator.clipboard) {
-            navigator.clipboard.writeText(link).then(() => {
-              console.log('Copied to clipboard successfully.');
-                var x = document.getElementById("snackbar");
-                x.className = "show";
-                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
-            }, (err) => {
-              console.log('Failed to copy the text to clipboard.', err);
-            });
-          } else if (window.clipboardData) {
-            window.clipboardData.setData("Text", input);
-          }
+        if (link) {
+
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(link).then(() => {
+                    console.log('Copied to clipboard successfully.');
+                    var x = document.getElementById("snackbar");
+                    x.className = "show";
+                    setTimeout(function () { x.className = x.className.replace("show", ""); }, 1000);
+                }, (err) => {
+                    console.log('Failed to copy the text to clipboard.', err);
+                });
+            } else if (window.clipboardData) {
+                window.clipboardData.setData("Text", input);
+            }
+        } else {
+            var x = document.getElementById("toasty");
+            x.className = "show";
+            setTimeout(function () { x.className = x.className.replace("show", ""); }, 1000);
+        }
     });
 });
